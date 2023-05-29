67114cc9232888951dcdcd6cfe6fdddd73f58c99
ysykzheng <1020680508@qq.com>
2023-05-29 09:57:01 +0800

    init

--- book.sh
+++ book.sh
@@ -1,7 +1,7 @@
 npm install
-npx honkit epub ./ career-advancement-opportunities-offering-clear-paths-for-growth-and-promotion.epub
+npx honkit epub ./ career-coaching-for-success-tools-and-techniques-for-maximizing-your-potential.epub
 
-ebook-convert career-advancement-opportunities-offering-clear-paths-for-growth-and-promotion.epub career-advancement-opportunities-offering-clear-paths-for-growth-and-promotion.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
+ebook-convert career-coaching-for-success-tools-and-techniques-for-maximizing-your-potential.epub career-coaching-for-success-tools-and-techniques-for-maximizing-your-potential.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
 
 # brew install pdftk-java
-java -jar "/usr/local/Cellar/pdftk-java/3.3.3/libexec/pdftk-all.jar" career-advancement-opportunities-offering-clear-paths-for-growth-and-promotion.pdf cat 2-end output career-advancement-opportunities-offering-clear-paths-for-growth-and-promotion-FINAL.pdf
+java -jar "/usr/local/Cellar/pdftk-java/3.3.3/libexec/pdftk-all.jar" career-coaching-for-success-tools-and-techniques-for-maximizing-your-potential.pdf cat 2-end output career-coaching-for-success-tools-and-techniques-for-maximizing-your-potential-FINAL.pdf
